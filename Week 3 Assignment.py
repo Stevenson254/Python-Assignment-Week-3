d5149e2df777b146b5dcfddbd898178092ee26d4
Stevenson254 <odhiambosteven761@gmail.com>
2025-04-01 14:45:38 +0300

    Third Commit

--- Week 3 Assignment.py
+++ Week 3 Assignment.py
@@ -7,7 +7,7 @@ def calculate_discount(price, discount_percent):
         return price
 
 # Prompt the user for input
-
+try:
     original_price = float(input("Enter the original price of the item: "))
     discount_percentage = float(input("Enter the discount percentage: "))
 
